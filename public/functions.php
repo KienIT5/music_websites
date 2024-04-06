788bfb2506785fdcd6c07e0a206d10aedce1745f
KienIT5 <93992503+KienIT5@users.noreply.github.com>
2024-04-06 16:31:04 +0700

    updatecsdl

--- public/functions.php
+++ public/functions.php
@@ -5,7 +5,7 @@
 	define("DBHOST", "localhost");
 	define("DBUSER", "root");
 	define("DBPASS", "");
-	define("DBNAME", "musicweb");
+	define("DBNAME", "musicwebupdate");
 	function db_connect(){
 		$string = DBDRIVER.":hostname=".DBHOST.";dbname=".DBNAME;
 		$con = new PDO($string, DBUSER, DBPASS);
@@ -48,89 +48,98 @@
 	}
 	// tu day la code kien xu ly index---------------------------------------------------------------------
 	function get_user_playlists($uid) {
-		$query = "SELECT user_playlists.pid, playlist_name
+		$query = "SELECT user_playlists.pid, playlist.playlist_name
 				FROM user_playlists
 				JOIN playlist ON user_playlists.pid = playlist.pid
 				WHERE uid = ?";
 		return db_query($query, array($uid));
 	}
+	
 	function get_albums() {
-		$query = "SELECT abid, title, album_image
+		$query = "SELECT albums.abid, albums.title, albums.album_image
 				FROM albums
-				WHERE status = 1
-				GROUP BY abid";
+				INNER JOIN songs ON albums.sid = songs.id
+				WHERE albums.status = 1
+				GROUP BY albums.abid";
 		return db_query($query);
 	}
+	
 	function get_artists() {
-		$query = "SELECT aid, artist_name, artist_image
+		$query = "SELECT id AS aid, name AS artist_name, image AS artist_image
 				FROM artists";
 		return db_query($query);
 	}
+	
 	function get_songs_by_playlist($pid) {
-		$query = "SELECT songs.sid, songs.title, artists.artist_name, songs.song_image, songs.file_path
+		$query = "SELECT songs.id AS sid, songs.title, artists.name AS artist_name, songs.image AS song_image, songs.file AS file_path
 				FROM songs
-				INNER JOIN user_playlists ON songs.sid = user_playlists.sid
-				INNER JOIN artists ON songs.aid = artists.aid
+				INNER JOIN user_playlists ON songs.id = user_playlists.sid
+				INNER JOIN artists ON songs.artist_id = artists.id
 				WHERE user_playlists.pid = ?";
 		return db_query($query, array($pid));
 	}
+	
 	function get_songs_by_album($abid) {
-		$query = "SELECT songs.sid, songs.title, artists.artist_name, songs.song_image, songs.file_path
+		$query = "SELECT songs.id AS sid, songs.title, artists.name AS artist_name, songs.image AS song_image, songs.file AS file_path
 				FROM songs
-				INNER JOIN albums ON songs.sid = albums.sid
-				INNER JOIN artists ON songs.aid = artists.aid
+				INNER JOIN albums ON songs.id = albums.sid
+				INNER JOIN artists ON songs.artist_id = artists.id
 				WHERE abid = ?";
 		return db_query($query, array($abid));
 	}
+	
 	function get_songs_by_artist($aid) {
-		$query = "SELECT songs.sid, songs.title, artist_name, songs.song_image, songs.file_path
+		$query = "SELECT songs.id AS sid, songs.title, artists.name AS artist_name, songs.image AS song_image, songs.file AS file_path
 				FROM songs 
-				INNER JOIN artists ON songs.aid = artists.aid
-				WHERE songs.aid = ?";
+				INNER JOIN artists ON songs.artist_id = artists.id
+				WHERE songs.artist_id = ?";
 		return db_query($query, array($aid));
 	}
+	
 	function get_favorite_songs($uid) {
-		$query = "SELECT songs.sid, songs.title, artists.artist_name, songs.song_image, songs.file_path
+		$query = "SELECT songs.id AS sid, songs.title, artists.name AS artist_name, songs.image AS song_image, songs.file AS file_path
 				FROM user_playlists
-				INNER JOIN songs ON user_playlists.sid = songs.sid
-				INNER JOIN artists ON songs.aid = artists.aid
+				INNER JOIN songs ON user_playlists.sid = songs.id
+				INNER JOIN artists ON songs.artist_id = artists.id
 				WHERE user_playlists.uid = ? AND user_playlists.favorite = 1";
 		return db_query($query, array($uid));
 	}
 	
 	function get_top_songs($limit) {
-		$query = "SELECT songs.sid, songs.title, artists.artist_name, songs.song_image, songs.file_path
+		$query = "SELECT songs.id AS sid, songs.title, artists.name AS artist_name, songs.image AS song_image, songs.file AS file_path
 				FROM user_playlists
-				INNER JOIN songs ON user_playlists.sid = songs.sid
-				INNER JOIN artists ON songs.aid = artists.aid
-				GROUP BY songs.sid
+				INNER JOIN songs ON user_playlists.sid = songs.id
+				INNER JOIN artists ON songs.artist_id = artists.id
+				GROUP BY songs.id
 				ORDER BY COUNT(user_playlists.sid) DESC
 				LIMIT ?";
 		return db_query($query, array($limit));
 	}
+	
 	function get_all_songs(){
-		$query = "SELECT songs.sid, songs.title, artists.artist_name, songs.song_image, songs.file_path
-		FROM songs
-		INNER JOIN artists ON songs.aid = artists.aid";
+		$query = "SELECT songs.id AS sid, songs.title, artists.name AS artist_name, songs.image AS song_image, songs.file AS file_path
+				FROM songs
+				INNER JOIN artists ON songs.artist_id = artists.id";
 		return db_query($query);
 	}
+	
 	function displaySongs($songs) {
-        if ($songs !== false) {
-            $count = 1;
-            foreach ($songs as $song) {
-                $count_str = str_pad($count, 2, '0', STR_PAD_LEFT);
-                echo "<li class='songItem' onclick='loadSong(\"{$song['title']}\", \"{$song['artist_name']}\",
-                    \"{$song['song_image']}\", \"{$song['file_path']}\")'>";
-                echo "<span>{$count_str}</span>";
-                echo "<img src='{$song['song_image']}' alt=''>";
-                echo "<h5>{$song['title']} <br> <div class='subtitle'>{$song['artist_name']}</div></h5>";
-                echo "<i class='bi playlistPlay bi-play-fill' id='{$song['sid']}'></i>";
-                echo "</li>";
-                $count++;
-            }
-        } else {
-            echo "<p>No songs available.</p>";
-        }
-    }
+		if ($songs !== false) {
+			$count = 1;
+			foreach ($songs as $song) {
+				$count_str = str_pad($count, 2, '0', STR_PAD_LEFT);
+				echo "<li class='songItem' onclick='loadSong(\"{$song['title']}\", \"{$song['artist_name']}\",
+					\"{$song['song_image']}\", \"{$song['file_path']}\")'>";
+				echo "<span>{$count_str}</span>";
+				echo "<img src='{$song['song_image']}' alt=''>";
+				echo "<h5>{$song['title']} <br> <div class='subtitle'>{$song['artist_name']}</div></h5>";
+				echo "<i class='bi playlistPlay bi-play-fill' id='{$song['sid']}'></i>";
+				echo "</li>";
+				$count++;
+			}
+		} else {
+			echo "<p>No songs available.</p>";
+		}
+	}	
 
 ?>
