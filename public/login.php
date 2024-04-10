ec5ec3f6cb99934ad5f0859d05d2cde369fdb514
KienIT5 <93992503+KienIT5@users.noreply.github.com>
2024-04-10 22:29:10 +0700

    updatelogin

--- public/login.php
+++ public/login.php
@@ -12,8 +12,7 @@ include "../app/core/functions.php";
 include "../app/core/config.php";
 include "../app/pages/includes/header.php";
 
-if($_SERVER['REQUEST_METHOD'] == "POST")
-{
+if($_SERVER['REQUEST_METHOD'] == "POST") {
     $values = [];
     $values['username'] = trim($_POST['username']);
     $password = trim($_POST['password']);
@@ -21,21 +20,43 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     if(empty($password)) {
         message("Password is required");
     } else {
-        $query = "select * from users where username = :username limit 1";
-        $row = db_query_one($query,$values);
+        $query = "SELECT * FROM users WHERE username = :username LIMIT 1";
+        $row = db_query_one($query, $values);
 
-        if(!empty($row) && $password == $row['password'])
-        {
+        if(!empty($row) && $password == $row['password']) {
             authenticate($row);
             message("Login successful");
-            redirect('admin');  				
-        }
-        else
-        {
+            
+            // Kiểm tra vai trò của người dùng và chuyển hướng tương ứng
+            if ($row['role'] == 'admin') {
+                redirect('admin');
+            } else {
+                redirect('home.php'); 
+            }
+        } else {
             message("Wrong username or password"); 
         }
     }
 }
+
+?>
+	<section class="content">
+ 
+		<div class="login-holder">
+
+		<?php if(message()):?>
+			<div class="alert"><?=message('',true)?></div>
+		<?php endif;?>
+
+			<form method="post">
+				<h2>Login</h2>
+				<input value="<?=set_value('username')?>" class="my-1 form-control" type="text" name="username" placeholder="Username">
+				<input value="<?=set_value('password')?>" class="my-1 form-control" type="password" name="password" placeholder="Password" >
+				<button class="my-1 btn " style="background-color:#5755FE;  border-radius: 5rem; color:#f7f7f7">Login</button>
+			</form>
+		</div>
+	</section>
+    <?php include "../app/pages/includes/footer.php";?>
 ?>
 	<section class="content">
  
